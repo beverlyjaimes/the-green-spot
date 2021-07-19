c761d43c99f3f60e1e9e30498080b295e0b94f4d
Beverly Jaimes-Puente <beverly.jaimespuente@gmail.com>
2021-07-19 13:14:06 -0500

    navbar
    navbar before icon

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,29 +1,46 @@
-import { AppBar, createStyles, makeStyles, Theme, Toolbar} from "@material-ui/core";
+import { AppBar, Button, createStyles, makeStyles, Theme, Toolbar, Typography} from "@material-ui/core";
 import React from "react";
 import logo from "../../images/logo.png"
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       background: '#FFFFFF',
+      height: 100,
+      paddingLeft: 0,
     },
-    title: {
-      flexGrow: 1,
-    },
+    navbtn: {
+      textTransform: 'none',
+      fontSize: 15,
+
+        },
     logo: {
-        maxWidth: 260,
-        height: 200,
-    }
+      height: 'auto',
+      width: 300,
+      marginLeft: -30,
+        },
+    title: {
+          flexGrow: 1,
+        },
+    
+    
   }),
 );
 export default function Nav(){
     const classes = useStyles();
     return(
-      
-       <AppBar position="static" className={classes.root}>
-       <Toolbar >
-         <img src={logo} alt="Logo" className={classes.logo}/>
-       </Toolbar>
-     </AppBar>
+  
+      <AppBar position="static" className={classes.root}>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+        
+          <img src={logo} alt="Logo" className={classes.logo}/>
+          </Typography>
+           <Button className={classes.navbtn}>New Arrivals</Button>
+           <Button className={classes.navbtn}>Best Sellers</Button>
+           <Button className={classes.navbtn}>About Us</Button>
+           <Button className={classes.navbtn}>Cart</Button>
+        </Toolbar>
+      </AppBar>
 
     )
        

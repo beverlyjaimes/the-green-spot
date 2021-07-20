cd90498654ff39c9337e86b1394fe3ee6dd34c92
Beverly Jaimes-Puente <beverly.jaimespuente@gmail.com>
2021-07-20 13:55:20 -0500

    home page
    completed home page (need font)

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,8 +1,10 @@
-import { Button, createStyles, makeStyles, Paper, Theme } from "@material-ui/core";
+import { Button, createStyles, makeStyles, Theme } from "@material-ui/core";
 import { Grid } from "@material-ui/core";
 import React from "react";
 import three from "../../images/three.jpg"
 import two from "../../images/two.jpg"
+import one from "../../images/one.jpg"
+
 
 
 
@@ -11,39 +13,36 @@ const useStyles = makeStyles((theme: Theme) =>
     root: {
       flexGrow: 1,
     },
-    paper: {
-      padding: theme.spacing(2),
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
-    },
+
     text: {
-        fontSize: 51,
+        fontSize: 60,
         textAlign:'center',
-        margin: -10
+        margin: -6
       
     },
     imgone: {
-        background: 'green'
+        background: 'green',
+        height: 384,
+        position: 'absolute',
+        top: '9em',
+        right: '3em',
     },
     imgtwo: {
         background: 'blue',
-        height: '370px',
-        width: '246px'
-
-
+        height: 465,
+        position: 'absolute',
+        right: '17em',
+        top: '19em'
+       
     },
     imgthree: {
         background: 'red',
-        height: 360,
-        width: 436
+        height: 260,
+        position: 'absolute',
+        right: '28em',
+        top: "10em"
 
-
-    },
-    textSide: {
-        // marginTop:"103px", 
-        // paddingLeft: "74px", 
-        dispaly: 'flex', 
-justifyContent: 'center'    }
+}
     
     
   }),
@@ -53,38 +52,36 @@ export default function Home(){
     const classes = useStyles();
 
     return(
-        <div>
-            <Grid container spacing={3}>
+        <div >
+            <Grid container >
                 <Grid container xs={4}
-                direction="column"
-                alignItems="center"
+                direction="column"                alignItems="center"
                 justify="center"
-                style={{ minHeight: '100vh' }} 
+                style={{ minHeight: '100vh',  paddingBottom: "12em" }} 
 
   >
                     {/* <Box display="flex" justifyContent="center"> */}
                     <h1 className={classes.text}>CREATE</h1>
-                    <h1 className={classes.text} style={{color: "#7FA63F", paddingLeft: "70px"}}>YOUR</h1>
+                    <h1 className={classes.text} style={{color: "#7FA63F", paddingLeft: "90px"}}>YOUR</h1>
                     <h1 className={classes.text} style={{paddingRight:"23px"}} >GREEN</h1>
-                    <h1 className={classes.text} style={{paddingLeft:"70px"}}>SPOT</h1>
-                    <h1 className={classes.text} >
-                    <Button variant="contained" style={{background:"#7FA63F", color:"white", fontSize: 23, marginTop:"51px", width:"200px", height:"43px"}} href="#contained-buttons">
+                    <h1 className={classes.text} style={{paddingLeft:"90px"}}>SPOT</h1>
+
+                    <div className={classes.text} style={{paddingLeft: "20px"}}>
+                    <Button variant="contained" style={{background:"#7FA63F", color:"white", fontSize: 23, marginTop:"58px", width:"225px", height:"43px"}} href="#contained-buttons">
                         SHOP NOW
                     </Button>
-                    </h1>
+                    </div>
                     {/* </Box> */}
 
                    
                 </Grid>
-                <Grid item xs={8}>
-                    <Paper className={classes.paper}>xs=6</Paper>
-                    <div>
-                        <img src={three} alt="three" className={classes.imgthree}/>
-                    </div>
-                    <div>
+                <Grid item xs={8} style={{paddingTop: "5em", paddingLeft: "5em"}}>
+                    <div >
+                    <img src={three} alt="three" className={classes.imgthree}/>
+                  
                     <img src={two} alt="three" className={classes.imgtwo}/>
+                    <img src={one} alt="three" className={classes.imgone}/>
                     </div>
-                    <div></div>
 
                 </Grid>
                   
